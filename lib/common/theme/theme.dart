77dea0e502690561df7b293545e4e247677ef827
Syntax0642 <calvinapriliohariyanto04@gmail.com>
2023-11-20 08:48:12 +0700

    regsiter_page [CA]

---
+++ lib/common/theme/theme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class ColorTheme{
+  static const Color purple = Color(0xFFFFFFFF);
+  static const Color red = Color(0xFFCC0202);
+  static const Color black = Color(0xFF000000);
+}
